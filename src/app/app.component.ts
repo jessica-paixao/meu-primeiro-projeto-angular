aa37ecceee7377c12f9c7e985e703fb130f3722a
jessicasouza-paixao <jessicapaixao.contato1@gmail.com>
2024-11-17 18:40:21 -0300

    adicionar formulario de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ImportsModule } from './imports';
 
-interface Usuario{
-    id?: number;
-    nome: string;
-    email: string;
-    telefone: string;
+interface Usuario {
+  id?: number;
+  nome: string;
+  email: string;
+  telefone: string;
 }
 
 @Component({
@@ -13,28 +13,49 @@ interface Usuario{
   standalone: true,
   imports: [ImportsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-    usuarios!: Usuario[];
+  nomeDoUsuario!: string;
+  emailDoUsuario!: string;
+  telefoneDoUsuario!: string;
 
-    constructor() {}
+  usuarios!: Usuario[];
 
-    ngOnInit() {
-        this.usuarios=[
-            {
-                id: 1, 
-                nome: "Jéssica Paixao",
-                email: "jessicapaixao@gmail.com",
-                telefone: "(22) 9 8827-9268"
-            },
-            {
-                id: 2, 
-                nome: "Julia Lima",
-                email: "julia@gmail.com",
-                telefone: "(62) 9 8427-9568"
+  constructor() {}
 
-            }
-        ]
-    }
+  ngOnInit() {
+    this.usuarios = [
+      {
+        id: 1,
+        nome: 'Jéssica Paixao',
+        email: 'jessicapaixao@gmail.com',
+        telefone: '(22) 9 8827-9268',
+      },
+      {
+        id: 2,
+        nome: 'Julia Lima',
+        email: 'julia@gmail.com',
+        telefone: '(62) 9 8427-9568',
+      },
+    ];
+  }
+
+  adicionarUsuario() {
+    const usuario: Usuario = {
+      id: this.usuarios.length + 1,
+      nome: this.nomeDoUsuario,
+      email: this.emailDoUsuario,
+      telefone: this.telefoneDoUsuario,
+    };
+
+    this.usuarios.push(usuario);
+    this.limparFormulario()
+  }
+
+  limparFormulario() {
+    this.nomeDoUsuario = '';
+    this.emailDoUsuario = '';
+    this.telefoneDoUsuario = '';
+  }
 }
